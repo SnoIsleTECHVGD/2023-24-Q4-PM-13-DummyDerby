d38c3757068bce82daf46ed4f0b6f98e6ec7aae7
MUKILTEO\1713514 <1713514@mukilteo.wednet.edu>
2024-04-11 12:53:02 -0700

    Lap Times and Checkpoints, fixed camera follower

--- Q4Q4Q4/Assets/Scenes/Scripts/Player.cs
+++ Q4Q4Q4/Assets/Scenes/Scripts/Player.cs
@@ -13,7 +13,7 @@ public class Player : MonoBehaviour
     public float CurrentLapTime { get; private set; } = 0;
     public int CurrentLap {  get; private set; } = 0;
 
-    private float lapTimer;
+    private float lapTimerTimestamp;
     private int lastCheckpointPassed = 0;
 
     private Transform checkpointsParent;
@@ -31,14 +31,49 @@ public class Player : MonoBehaviour
 
     void StartLap()
     {
+        Debug.Log("StartLap!");
         CurrentLap++;
         lastCheckpointPassed = 1;
-        lapTimer = Time.deltaTime;
+        lapTimerTimestamp = Time.time;
+    }
+
+    void EndLap()
+    {
+        LastLapTime = Time.time - lapTimerTimestamp;
+        BestLapTime = Mathf.Min(LastLapTime, BestLapTime);
+        Debug.Log("EndLap - LapTime was " + LastLapTime + "seconds");
+    }
+    
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.layer != checkpointLayer)
+        {
+            return;
+        }
+
+        if ( collider.gameObject.name == "1")
+        {
+            if ( lastCheckpointPassed == checkpointCount)
+            {
+                EndLap();
+            }
+
+            if (CurrentLap == 0 || lastCheckpointPassed == checkpointCount)
+            {
+                StartLap();
+            }
+            return;
+        }
+
+        if ( collider.gameObject.name == (lastCheckpointPassed+1).ToString())
+        {
+            lastCheckpointPassed++;
+        }
     }
     // Update is called once per frame
     void Update()
     {
-        CurrentLapTime = lapTimer > 0 ? Time.deltaTime - lapTimer : 0; 
+        CurrentLapTime = lapTimerTimestamp > 0 ? Time.time - lapTimerTimestamp : 0; 
         if  (controlType == ControlType.HumanInput)
         {
             carController.Steer = GameManager.Instance.InputController.SteerInput;
