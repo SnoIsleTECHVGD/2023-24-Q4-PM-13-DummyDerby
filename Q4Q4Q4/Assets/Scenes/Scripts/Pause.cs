0a4559d77525b300fdf57a7b34c5432eb66b03c5
Aldo127 <113369623+Aldo127@users.noreply.github.com>
2024-04-14 23:48:40 -0700

    Start screen

--- Q4Q4Q4/Assets/Scenes/Scripts/Pause.cs
+++ Q4Q4Q4/Assets/Scenes/Scripts/Pause.cs
@@ -1,18 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Pause : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public static bool isGamePaused = false;
 
-    // Update is called once per frame
+    [SerializeField] GameObject pauseMenu;
+
+
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isGamePaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+
+    public void ResumeGame()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        isGamePaused = false;
+        Debug.Log("Resume");
+    }
+
+    void PauseGame()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        isGamePaused = true;
+
+        Debug.Log("Paused");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+
+        Debug.Log("Quit");
     }
 }
